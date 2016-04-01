ad2c60d386d8870529da15ed1eb9960c7e6c3f4c
Naveen <p6naveen@gmail.com>
2016-04-01 20:11:00 +0530

    dataprovider added in test class

--- tests/MathTest.php
+++ tests/MathTest.php
@@ -8,9 +8,28 @@ namespace BuggyAppTest;
 
 class MathTest extends \PHPUnit_Framework_TestCase {
 
-    public function testAddNumbers() {
-        $obj = new \BuggyApp\Math();
-        $this->assertEquals(5, $obj->Sum(2, 3));
+    public function Setup() {
+        $this->math = new \BuggyApp\Math();
+    }
+
+    public function inputNumbers() {
+        return [
+            [2, 2, 4],
+            [2.5, 2.5, 5],
+            [-3, 1, -2],
+            [-9, -9, -18],
+        ];
+    }
+
+    /**
+     * 
+     * @param type $a
+     * @param type $b
+     * @param type $sum
+     * @dataProvider inputNumbers
+     */
+    public function testAddNumbers($a, $b, $sum) {
+        $this->assertEquals($sum, $this->math->Sum($a, $b));
     }
 
 }
